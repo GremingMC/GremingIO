22af210a52eb40fbc869efeb048a549b130de68d
RomnSD <RomnSDN@gmail.com>
2021-06-11 17:20:22 -0400

    Added GremingIO

--- src/main/java/com/greming/io/ByteBuffer.java
+++ src/main/java/com/greming/io/ByteBuffer.java
@@ -1,12 +1,40 @@
+/**
+ * 
+ *    ____                    _              
+ *   / ___|_ __ ___ _ __ ___ (_)_ __   __ _  
+ *  | |  _| '__/ _ \ '_ ` _ \| | '_ \ / _` | 
+ *  | |_| | | |  __/ | | | | | | | | | (_| | 
+ *   \____|_|  \___|_| |_| |_|_|_| |_|\__, | 
+ *                                    |___/  
+ * 
+ * This file is part of Greming.
+ * 
+ * Greming is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Greming is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Greming. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @author Brayan Roman
+ * 
+ */
+
 package com.greming.io;
 
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 
-/**
- * @author RomnSD
- */
-public class ByteBuffer
+public class ByteBuffer implements Cloneable
 {
     
     
@@ -53,7 +81,7 @@ public class ByteBuffer
      * @param offset int
      * @return       byte 
      */
-    public byte get(int offset) { return buffer[offset]; }
+    public byte get(int offset) { return offset >= buffer.length ? (byte) 0 : buffer[offset]; }
     
     
     /**
@@ -63,6 +91,9 @@ public class ByteBuffer
      */
     public byte[] get(int offset, int size)
     {
+        if (size > buffer.length)
+            return new byte[0];
+        
         byte[] result = new byte[size];
         System.arraycopy(buffer, offset, result, 0, size);
         
@@ -89,7 +120,7 @@ public class ByteBuffer
      */
     public void expand(int size)
     {
-        if (size > 0)
+        if (size > buffer.length)
             buffer = Arrays.copyOf(buffer, size);
     }
     
@@ -99,6 +130,93 @@ public class ByteBuffer
      * @return      boolean 
      */
     public boolean equals(ByteBuffer other) { return Arrays.equals(buffer, other.getBytes()); }
+    
+    
+    /**
+     * @return ByteBuffer
+     */
+    public ByteBuffer clone() { return new ByteBuffer(Arrays.copyOf(buffer, buffer.length)); }
+    
+    
+    /**
+     * @param buffer byte[]
+     * @return       byte[]
+     */
+    public static byte[] encodeBase64(byte[] buffer)
+    {
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encode(buffer);
+    }
+    
+    
+    /**
+     * @param buffer byte[]
+     * @return       byte[]
+     */
+    public static byte[] decodeBase64(byte[] buffer)
+    {
+        Base64.Decoder decoder = Base64.getDecoder();
+        return decoder.decode(buffer);
+    }
+    
+    
+    /**
+     * @param buffer           byte[]
+     * @param compressionLevel int 
+     * @return 
+     */
+    public static byte[] encodeZip(byte[] buffer, int compressionLevel) { return encodeZip(buffer, 1024 * 1024 * 2, compressionLevel); }
+    
+    
+    /**
+     * @param buffer           byte[]
+     * @param outSize          int 
+     * @param compressionLevel int
+     * @return                 byte[]
+     */
+    public static byte[] encodeZip(byte[] buffer, int outSize , int compressionLevel)
+    {
+        Deflater deflater = new Deflater(compressionLevel, true);
+        deflater.setInput(buffer);
+        deflater.finish();
+        
+        byte[] output = new byte[outSize];
+        deflater.deflate(output);
+        deflater.end();
+        
+        return output;
+    }
+    
+    
+    /**
+     * @param buffer           byte[]
+     * @param compressionLevel int 
+     * @return 
+     */
+    public static byte[] decodeZip(byte[] buffer, int compressionLevel) { return encodeZip(buffer, 1024 * 1024 * 2, compressionLevel); }
+    
+    
+    /**
+     * @param buffer           byte[]
+     * @param outSize          int 
+     * @param compressionLevel int 
+     * @return                 byte[]
+     */
+    public static byte[] decodeZip(byte[] buffer, int outSize , int compressionLevel)
+    {
+        Inflater inflater = new Inflater(true);
+        inflater.setInput(buffer);
+        
+        byte[] output = new byte[outSize];
+        
+        try { output = Arrays.copyOf(output, inflater.inflate(output)); } catch (Throwable exception) { 
+            exception.printStackTrace();
+        }
+        
+        inflater.end();
+         
+       return output;
+    }
     
     
 }
--- src/test/java/com/greming/io/ByteBufferTest.java
+++ src/test/java/com/greming/io/ByteBufferTest.java
@@ -1,25 +1,49 @@
-package com.greming.io;
+/**
+ * 
+ *    ____                    _              
+ *   / ___|_ __ ___ _ __ ___ (_)_ __   __ _  
+ *  | |  _| '__/ _ \ '_ ` _ \| | '_ \ / _` | 
+ *  | |_| | | |  __/ | | | | | | | | | (_| | 
+ *   \____|_|  \___|_| |_| |_|_|_| |_|\__, | 
+ *                                    |___/  
+ * 
+ * This file is part of Greming.
+ * 
+ * Greming is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Greming is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Greming. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @author Brayan Roman
+ * 
+ */
 
-import java.util.Arrays;
+package com.greming.io;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
-import com.greming.io.*;
 
 
 public class ByteBufferTest
 {
     
     @Test
-    public void bufferTest()
+    public void test()
     {
-        validate(ByteBufferType.VarInt);
-        validate(ByteBufferType.LittleEndian);
-        validate(ByteBufferType.BigEndian);
+        test(ByteBufferType.VarInt);
+        test(ByteBufferType.LittleEndian);
+        test(ByteBufferType.BigEndian);
     }
     
-    private void validate(ByteBufferType type)
+    private void test(ByteBufferType type)
     {
         ByteBuffer buffer = new ByteBuffer(1);
         
@@ -53,6 +77,9 @@ public class ByteBufferTest
         byte[] bytes = new byte[] { 1, 1, 1, 0, 1, 1, 0, 1 };
         writer.put(bytes);
         
+        String string = "ByteBuffer TEST";
+        writer.writeString(string);
+        
         
         Assertions.assertEquals(byte8, reader.readByte());
         Assertions.assertEquals(booleanTrue, reader.readBoolean());
@@ -62,7 +89,8 @@ public class ByteBufferTest
         Assertions.assertEquals(int64, reader.readInt64());
         Assertions.assertEquals(float32, reader.readFloat32());
         Assertions.assertEquals(float64, reader.readFloat64());
-        Assertions.assertTrue(Arrays.equals(bytes, reader.get(bytes.length)));
+        Assertions.assertArrayEquals(bytes, reader.get(bytes.length));
+        Assertions.assertEquals(string, reader.readString());
     }
     
     
